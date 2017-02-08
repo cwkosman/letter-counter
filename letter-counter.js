6fceaee5dfc0a4a6d1e0d1dec7759b0188968600
Chuck Kosman <cwkosman@gmail.com>
2017-02-08 20:08:02 +0000

    Refactored with Dave: Array.from() and others

--- letter-counter.js
+++ letter-counter.js
@@ -1,19 +1,11 @@
-function countLetters(string) {
-  //create an array whose entires are each of the letters of the string, minus spaces
-  var singleLetters = string.replace(/ /g,'').split('');
-  //use the reduce function to pull of a sweet loop
-  //reduce takes a very specific callback function structure and is passed an empty initial object
-  var counter = singleLetters.reduce((letterTally, letter) => {
-    //create the letter as a key if it is not yet there
+function countLetters(str) {
+  return Array.from(str.replace(/\s+/g, '')).reduce((letterTally, letter) => {
     if (!letterTally[letter]) {
       letterTally[letter] = 0;
     }
-    //increment an existing letter property by 1
     letterTally[letter]++;
-    //return the object so it can be handled by .reduce again
     return letterTally;
   }, {});
-  console.log(counter);
 }
 
-countLetters("lighthouse in the house");
+console.log(countLetters("lighthouse in the house"));
